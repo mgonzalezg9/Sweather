1cf52dd8f62842d99ef11cb4ca7a1c69b578556a
Manuel Gonzalez <mgonzalez4648@gmail.com>
2024-04-16 19:19:46 +0200

    Fix time formatting in LocationDetails screen

--- src/hooks/weather/useWeatherForecast.ts
+++ src/hooks/weather/useWeatherForecast.ts
@@ -1,46 +1,73 @@
 import { getCurrentWeather, getHourlyForecast } from '@/services/weather';
 import { Coordinates, Forecast, Weather } from '@/services/weather/types';
 import { useEffect, useState } from 'react';
+import useDeviceLocales from './useDeviceLocales';
 
 export type LocationQuery = {
     location: string;
     coordinates: Coordinates;
+    device: {
+        locale: string;
+        timeZone?: string;
+    }
 };
 
-const getWeatherAndForecast = async ({ location, coordinates }: LocationQuery) => {
+const getWeatherAndForecast = async ({ location, coordinates, device }: LocationQuery) => {
     const [weather, forecast] = await Promise.all([
-        getCurrentWeather({ location, coordinates }),
-        getHourlyForecast({ location, coordinates })
+        getCurrentWeather({
+            location,
+            coordinates,
+            device
+        }),
+        getHourlyForecast({
+            location,
+            coordinates,
+            device
+        })
     ])
 
     return { weather, forecast };
 }
 
 export default function useWeatherForecast({ location, coordinates }: LocationQuery) {
+    const { timeZone, locale } = useDeviceLocales();
+
     const [weather, setWeather] = useState<Weather>();
     const [forecast, setForecast] = useState<Forecast>();
-
     const [isLoading, setLoading] = useState<boolean>(false);
     const [isError, setError] = useState<unknown>();
 
     useEffect(() => {
-        if (!location && !coordinates) {
-            return;
+        const loadWeather = async () => {
+            if (!location && !coordinates) {
+                return;
+            }
+
+            console.log("Requesting weather at location", location || coordinates);
+            setLoading(true);
+
+            try {
+                const { weather, forecast } = await getWeatherAndForecast({
+                    location,
+                    coordinates,
+                    device: {
+                        locale,
+                        timeZone
+                    }
+                })
+
+                setWeather(weather);
+                setForecast(forecast);
+            } catch (error) {
+                console.error(`Unable to retrieve weather at location ${location || coordinates}`);
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
         }
 
-        console.log("Requesting weather at location", location || coordinates);
-        setLoading(true);
-
-        getWeatherAndForecast({ location, coordinates }).then(({ weather, forecast }) => {
-            setWeather(weather);
-            setForecast(forecast);
-        }).catch(error => {
-            console.error(`Unable to retrieve weather at location ${location || coordinates}`);
-            setError(error);
-        }).finally(() => {
-            setLoading(false);
-        });
-    }, [location, coordinates]);
+        loadWeather();
+    }, [location, coordinates, timeZone, locale]);
 
     return {
         weather,
