cf3b518a43e621d649913e074908ce11b266f8ca
Manuel Gonzalez <mgonzalez4648@gmail.com>
2024-05-30 09:54:23 +0200

    Imrpove time tests names

--- src/weather/utils/__tests__/time.spec.ts
+++ src/weather/utils/__tests__/time.spec.ts
@@ -7,27 +7,25 @@ import {
 
 describe("Weather Service Time formatter", () => {
   const esLocale = "es-ES";
-  it.each(ES_LOCALE_TEST_SUITE)(
-    `should format time for ${esLocale}`,
-    (time, result) => {
+  ES_LOCALE_TEST_SUITE.forEach(([time, result]) =>
+    it(`should format time "${result}" for ${esLocale}`, () => {
       expect(formatDate({ time, locale: esLocale })).toBe(result);
-    }
+    })
   );
 
   const enLocale = "en-US";
-  it.each(EN_LOCALE_TEST_SUITE)(
-    `should format time for ${enLocale}`,
-    (time, result) => {
+  EN_LOCALE_TEST_SUITE.forEach(([time, result]) =>
+    it(`should format time "${result}" for ${enLocale}`, () => {
       expect(formatDate({ time, locale: enLocale })).toBe(result);
-    }
+    })
   );
 
   IS_DAY_TEST_SUITE.forEach(({ time, sunrise, sunset, result }) => {
-    it(`should correctly determine if it is day time for ${formatDate({
+    it(`should correctly determine if ${formatDate({
       time,
       locale: enLocale,
       timeZone: "UTC",
-    })}`, () => {
+    })} has day light`, () => {
       expect(
         hasDaylight({
           time,
