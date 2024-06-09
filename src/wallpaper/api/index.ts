67fe2bae57f7f0a29859b12ee5bcfdad906768eb
Manuel Gonzalez <mgonzalez4648@gmail.com>
2024-06-09 17:51:47 +0200

    Fix Expo APK generation build error

--- src/wallpaper/api/index.ts
+++ src/wallpaper/api/index.ts
@@ -1,10 +1,13 @@
 import * as httpClient from "@/utils/httpClient";
 import { WallpaperEndpoint } from "./endpoints";
 
-const {
-  EXPO_PUBLIC_UNSPLASH_URL: API_URL,
-  EXPO_PUBLIC_UNSPLASH_API_KEY: API_KEY,
-} = process.env;
+// ! Forbidden according to https://docs.expo.dev/guides/environment-variables/#how-to-read-from-environment-variables
+// const {
+//   EXPO_PUBLIC_UNSPLASH_URL: API_URL,
+//   EXPO_PUBLIC_UNSPLASH_API_KEY: API_KEY,
+// } = process.env;
+const API_KEY = process.env.EXPO_PUBLIC_UNSPLASH_API_KEY;
+const API_URL = process.env.EXPO_PUBLIC_UNSPLASH_URL;
 
 export const wallpaperApi = {
   get: <T>(
